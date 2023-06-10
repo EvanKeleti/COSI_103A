6f4b4e1a75d600e6e4d735bcf9996ceb074eb70f
EvanKeleti <evankeleti@brandeis.edu>
2023-06-10 19:36:55 -0400

    changed methods to use professor's openai api and added poem and index routes

--- CA02/firstapp/routes/gpt.js
+++ CA02/firstapp/routes/gpt.js
@@ -1,49 +1,59 @@
 const express = require('express');
 const router = express.Router();
-const { Configuration, OpenAIApi } = require("openai");
 const axios = require('axios');
 
-// configure OpenAI library using api key from environment variable 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+/* 
+My openai api key expired, so I commented out the code using the openai package.
+*/
+
+// const { Configuration, OpenAIApi } = require("openai");
+
+// // configure OpenAI library using api key from environment variable 
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
+
+
+
+// router.post('/gpt', isLoggedIn, async (req, res, next) => {
+//   try {
+//     const completion = await openai.createCompletion({
+//       model: "text-davinci-003",
+//       prompt: req.body.prompt,
+//     });
+//     console.log(completion.data.choices[0].text);
+//   } catch (error) {
+//     if (error.response) {
+//       console.log(error.response.status);
+//       console.log(error.response.data);
+//     } else {
+//       console.log(error.message);
+//     }
+//   }
+// });
+
+router.get('/openai-demo', isLoggedIn, async (req, res, next) => {
+  const header = "Get a Response from OpenAI";
+  const promptStart = "";
+  res.render('openai', { header, promptStart });
 });
-const openai = new OpenAIApi(configuration);
 
-router.get('/openai_demo', isLoggedIn, async (req, res, next) => {
-  res.render('chatGPT');
-});
-
-router.post('/gpt', isLoggedIn, async (req, res, next) => {
-  try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: req.body.prompt,
-    });
-    console.log(completion.data.choices[0].text);
-  } catch (error) {
-    if (error.response) {
-      console.log(error.response.status);
-      console.log(error.response.data);
-    } else {
-      console.log(error.message);
-    }
-  }
-  // const completion = await openai.createCompletion({
-  //   model: "text-davinci-003",
-  //   prompt: req.body.prompt,
-  // });
-  // console.log(completion);
-  //const answer = completion.data.choices[0].text;
-  //res.render('gptAnswer', { answer });
-});
-
-
-router.post('/openai_demo', isLoggedIn, async (req, res, next) => {
+router.post('/openai-demo', isLoggedIn, async (req, res, next) => {
+  console.log(req.body.promptStart);
   const response = await axios.post('http://gracehopper.cs-i.brandeis.edu:3500/openai',
-    { prompt: req.body.prompt });
+    { prompt: req.body.promptStart + req.body.prompt });
   res.send(response.data);
-  // const answer = response.data.choices[0].message.content;
-  // res.render('gptAnswer', { answer });
+});
+
+router.get('/openai-poem', isLoggedIn, async (req, res, next) => {
+  const header = "Give OpenAI a Topic to Write a Poem About!"
+  const promptStart = "Write a poem about ";
+  res.render('openai', { header, promptStart })
+});
+
+router.get('/openai-index', isLoggedIn, async (req, res, next) => {
+  res.render('openaiIndex');
 });
 
 module.exports = router;
